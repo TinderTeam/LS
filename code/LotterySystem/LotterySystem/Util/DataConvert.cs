47bbf2a733c855d6c4dcf0048335bacf57b9e2b4
Zhuliucao <aether1989@gmail.com>
2014-08-26 23:26:55 +0800

    小修改

---
+++ code/LotterySystem/LotterySystem/Util/DataConvert.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LotterySystem.Util
+{
+    public class DataConvert
+    {
+        public static List<T> ConvertIListToList<T>(System.Collections.IList gbList) where T : class
+        {
+            if (gbList != null && gbList.Count >= 1)
+            {
+                List<T> list = new List<T>();
+                for (int i = 0; i < gbList.Count; i++)
+                {
+                    T temp = gbList[i] as T;
+                    if (temp != null)
+                        list.Add(temp);
+                }
+                return list;
+            }
+            return null;
+        }
+    }
+}
